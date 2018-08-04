c4353a11055a60f1b733641130028194a911bb53
NitinGahlawat11 <webnitin640@gmail.com>
2018-08-04 18:15:56 +0530

    skelton ready and inital js added

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+var playing=false;
+var timer=document.getElementById('timer');
+var start=document.getElementById('startreset');
+var timerval=document.getElementById('timerval');
+ var scorevalue=document.getElementById('scorevalue');
+var score;
+ start.onclick=function () {
+    if(playing){
+        location.reload(); // reload the page if user is playing
+    }
+    else{
+        score=1;
+        playing=true;
+        timer.style.display="block";
+        scorevalue.innerHTML=score;
+start.innerHTML="Reset Game";
 
+    }
+ }
