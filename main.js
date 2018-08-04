1a4f60c136f6f64c6dce34f14e44b048f9f4455c
NitinGahlawat11 <webnitin640@gmail.com>
2018-08-04 19:06:32 +0530

    timer function added

--- main.js
+++ main.js
@@ -3,17 +3,58 @@ var timer=document.getElementById('timer');
 var start=document.getElementById('startreset');
 var timerval=document.getElementById('timerval');
  var scorevalue=document.getElementById('scorevalue');
+ var gameover=document.getElementById('gameover');
 var score;
+var action;
+var timeRemaining;
  start.onclick=function () {
-    if(playing){
-        location.reload(); // reload the page if user is playing
-    }
-    else{
-        score=1;
-        playing=true;
-        timer.style.display="block";
-        scorevalue.innerHTML=score;
-start.innerHTML="Reset Game";
+     if (playing) {
+         location.reload(); // reload the page if user is playing
+     }
+     else {
+         score = 1;
+         playing = true;
+         timer.style.display = "block";
+         scorevalue.innerHTML = score;
+         start.innerHTML = " Reset Game";
+         timeRemaining = 10;
+//timerval.innerHTML=timeRemaining;
+         gameover.style.display="none";
 
-    }
+         startCountDown();
+
+         generateQA();
+
+
+     }
+
+
+     function startCountDown() {
+         var action = setInterval(function () {
+             timeRemaining -= 1;
+             timerval.innerHTML = timeRemaining;
+             if (timeRemaining == 0) { // if timer reaches 0 stop the timer and game is over
+                 stopCountDown();
+                 gameover.style.display = "block";
+                 gameover.innerHTML = "<p>Game Over</p><p>Your Score is" + score + "</p>"
+                 timer.style.display = "none";
+                 playing = false;
+                 start.innerHTML = "Start Game";
+             }
+         }, 1000)
+     }
+
+     function stopCountDown() {
+         clearInterval(action); // clear Interval fn stops the timer
+     }
+
+     function generateQA() {
+
+
+
+
+
+     }
+
+
  }
