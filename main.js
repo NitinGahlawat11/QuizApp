1afd6314692ad0c2ab5d01bd01329823532d186a
NitinGahlawat11 <webnitin640@gmail.com>
2018-08-07 18:13:13 +0530

    project ready

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ var start=document.getElementById('startreset');
 var timerval=document.getElementById('timerval');
  var scorevalue=document.getElementById('scorevalue');
  var gameover=document.getElementById('gameover');
+ var question=document.getElementById('question');
+ var correct=document.getElementById('correct');
+ var wrong= document.getElementById('wrong');
 var score;
 var action;
 var timeRemaining;
+var z;
  start.onclick=function () {
      if (playing) {
          location.reload(); // reload the page if user is playing
@@ -27,7 +31,37 @@ var timeRemaining;
 
 
      }
+     for(i=1; i<5; i++){
+         document.getElementById("box"+i).onclick = function(){
+             //check if we are playing
+             if(playing == true){//yes
+                 if(this.innerHTML == z){
+                     //correct answer
 
+                     //increase score by 1
+                     score++;
+                     document.getElementById("scorevalue").innerHTML = score;
+                     //hide wrong box and show correct box
+                     wrong.style.display="none";
+                     correct.style.display="block";
+                     setTimeout(function(){
+                         correct.style.display="none";
+                     }, 1000);
+
+                     //Generate new Q&A
+
+                     generateQA();
+                 }else{
+                     //wrong answer
+                     correct.style.display="none";
+                     wrong.style.display="block";
+                     setTimeout(function(){
+                         wrong.style.display="none";
+                     }, 1000);
+                 }
+             }
+         }
+     }
 
      function startCountDown() {
          var action = setInterval(function () {
@@ -50,9 +84,24 @@ var timeRemaining;
 
      function generateQA() {
 
+var x= 1+Math.round(9*Math.random());
+var y=1+Math.round(9*Math.random());
+ z= x*y;
+
+question.innerHTML=x + "* " + y;
+ // enter the correct answer at one of the box
+
+         var correctOption=1+Math.round(3*Math.random());
+         document.getElementById('box'+correctOption).innerHTML=z; // placing the z at any position
 
 
-
+         //fill the remaining options with wrong ans
+         for( i=1;i<5;i++){
+if(i!=correctOption){
+    var wrongAns=(1+Math.round(9*Math.random()))*(1+Math.round(9*Math.random()));
+document.getElementById('box'+i).innerHTML=wrongAns;
+}
+         }
 
      }
 
